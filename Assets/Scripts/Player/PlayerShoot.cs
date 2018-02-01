f2069ae681a71df8f27e8e7c613d8149699cafe6
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2018-02-01 00:55:14 -0500

    Made fighter not shoot when respawning

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class PlayerShoot : MonoBehaviour {
     //reference to bullet prefab
     public GameObject bullet;
-
+	bool canShoot;
     // Update is called once per frame
     void Update () {
+		canShoot = gameObject.GetComponentInChildren<Collider2D> ().isActiveAndEnabled;
         if (Input.GetKeyDown(KeyCode.Space)) {
-            Instantiate(bullet, new Vector3(transform.localPosition.x, transform.localPosition.y + 1f, 0), transform.rotation);
+			if (canShoot == true) {
+				Instantiate (bullet, new Vector3 (transform.localPosition.x, transform.localPosition.y + 1f, 0), transform.rotation);
+			}
         }
     }
 }
