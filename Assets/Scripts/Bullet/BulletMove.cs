4c5713f744d050548c3b3ad26d4d7a61b1792902
Hyacinth Nil <j.shepard.ramsay@gmail.com>
2018-01-31 21:26:05 -0500

    added player and main scene

---
+++ Assets/Scripts/Bullet/BulletMove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMove : MonoBehaviour {
+
+	public float bulletSpeed = 100f;
+	void Start(){
+		Destroy (gameObject, 3f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(new Vector3(0, bulletSpeed * Time.deltaTime, 0));
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+	public float speed = 10f;
+	public GameObject bullet;
+
+	private float xPos;
+	private float yPos;
+	// Update is called once per frame
+	void Update () {
+		xPos = transform.position.x;
+		yPos = transform.position.y;
+		transform.Translate (Input.GetAxis ("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0);
+		//mathf clamp
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Instantiate (bullet, new Vector3 (transform.localPosition.x, transform.localPosition.y + 1f, 0), transform.rotation);
+		}
+
+		if (transform.position.x > -6) {
+			//transform.position.x = -6;
+		}
+		else if(transform.position.x < 6){
+
+		}		
+
+		if(transform.position.y > -3){
+
+		}
+		else if(transform.position.y < 3){
+
+		}
+	}
+}
